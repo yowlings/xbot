4fb64cc58db6063e8e7d4f4fd114b63d0631431f
wangpeng <wangpeng@droid.ac.cn>
2020-08-10 21:23:24 +0800

    auto push github

--- .ci-runner.sh
+++ .ci-runner.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-git checkout melodic-devel
-rm -rf ~/catkin_ws/src/xbot
-cp -r ../xbot ~/catkin_ws/src
+cd ~/catkin_ws/src/
+rm -rf xbot
+git clone -b melodic-devel git@yt.droid.ac.cn:xbot-u/xbot.git
 cd ~/catkin_ws
 source /opt/ros/melodic/setup.bash
 rosdep install --from-paths src --ignore-src --rosdistro=melodic -y
-sleep 30
 catkin_make
+cd ~/catkin_ws/src/xbot/
+echo "[remote \"github\"]
+	url = git@github.com:DroidAITech/xbot.git
+	fetch = +refs/heads/*:refs/remotes/origin/*" >> .git/config
+git push github melodic-devel
