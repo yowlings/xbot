016946307bd8707bb6815a99504aa66f5cedade7
wangpeng <wangpeng@droid.ac.cn>
2020-08-24 14:11:07 +0800

    find const camera id

--- xbot_face/script/face_register.py
+++ xbot_face/script/face_register.py
@@ -27,7 +27,7 @@ def getCameraId():
   # print vs
   for dev in vs:
     vd = dev[:-1]
-    cmd = "udevadm info "+vd+" |grep USB_video_device > vs.info"
+    cmd = "udevadm info "+vd+" |grep 'ID_VENDOR_ID=0bda' > vs.info"
     os.system(cmd)
     infof = open("vs.info",'r')
     info = infof.readlines()
